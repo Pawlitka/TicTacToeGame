4e9baf8be397cfe6b3cb94b5b8257aace550faae
pawelkapusta <pawelllosek@gmail.com>
2025-02-20 19:23:31 +0100

    add textLabel and textPanel

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -7,13 +7,28 @@ public class TicTacToe {
     int boeardHeight = 650;
 
     JFrame frame = new JFrame();
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
 
     TicTacToe() {
         frame.setVisible(true);
         frame.setSize(boardWidth,boeardHeight);
         frame.setLocationRelativeTo(null);
-        frame.setResizable(true);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
+
+        textLabel.setBackground(Color.PINK);
+        textLabel.setForeground(Color.BLACK);
+        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Tic-Tac-Toe");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel);
+
     }
 }
